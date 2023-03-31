109659bc8c4ba20c7668bd83d21c21a74d432b7c
mutai-gilbert <mutaigilbert85@gmail.com>
2023-03-31 09:28:14 +0300

    Added new files and made some changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes, Link } from 'react-router-dom';
 import Calculator from './components/calculator';
 import FetchTheQuote from './api/quotes';
+import Home from './components/home';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,16 +13,35 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="quotesItems">
-        <div className="fitnessquote">
-          <FetchTheQuote />
+      <>
+        <nav
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+          className="navigation"
+        >
+          <h1>Math Magician</h1>
+          <ul style={{ listStyleType: 'none' }}>
+            <li>
+              <Link to="/" className="link">Home</Link>
+            </li>
+            <li>
+              <Link to="/calculator" className="link">Calculator</Link>
+            </li>
+            <li>
+              <Link to="/quote" className="link">Quote</Link>
+            </li>
+          </ul>
+        </nav>
 
-        </div>
-
-        <div className="calculator-wrap">
-          <Calculator />
-        </div>
-      </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<FetchTheQuote />} />
+        </Routes>
+      </>
     );
   }
 }
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -16,32 +16,34 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calculator">
-      <div className="button output">
-        {total}
-        {operation}
-        {next}
-      </div>
+    <div className="calculator-wrap">
+      <div className="calculator">
+        <div className="button output">
+          {total}
+          {operation}
+          {next}
+        </div>
 
-      <Button handleClick={handleClick} label="AC" />
-      <Button handleClick={handleClick} label="+/-" />
-      <Button handleClick={handleClick} label="%" />
-      <Button handleClick={handleClick} label="÷" />
-      <Button handleClick={handleClick} label="7" />
-      <Button handleClick={handleClick} label="8" />
-      <Button handleClick={handleClick} label="9" />
-      <Button handleClick={handleClick} label="x" />
-      <Button handleClick={handleClick} label="4" />
-      <Button handleClick={handleClick} label="5" />
-      <Button handleClick={handleClick} label="6" />
-      <Button handleClick={handleClick} label="-" />
-      <Button handleClick={handleClick} label="1" />
-      <Button handleClick={handleClick} label="2" />
-      <Button handleClick={handleClick} label="3" />
-      <Button handleClick={handleClick} label="+" />
-      <Button handleClick={handleClick} label="0" />
-      <Button handleClick={handleClick} label="." />
-      <Button handleClick={handleClick} label="=" />
+        <Button handleClick={handleClick} label="AC" />
+        <Button handleClick={handleClick} label="+/-" />
+        <Button handleClick={handleClick} label="%" />
+        <Button handleClick={handleClick} label="÷" />
+        <Button handleClick={handleClick} label="7" />
+        <Button handleClick={handleClick} label="8" />
+        <Button handleClick={handleClick} label="9" />
+        <Button handleClick={handleClick} label="x" />
+        <Button handleClick={handleClick} label="4" />
+        <Button handleClick={handleClick} label="5" />
+        <Button handleClick={handleClick} label="6" />
+        <Button handleClick={handleClick} label="-" />
+        <Button handleClick={handleClick} label="1" />
+        <Button handleClick={handleClick} label="2" />
+        <Button handleClick={handleClick} label="3" />
+        <Button handleClick={handleClick} label="+" />
+        <Button handleClick={handleClick} label="0" />
+        <Button handleClick={handleClick} label="." />
+        <Button handleClick={handleClick} label="=" />
+      </div>
     </div>
   );
 };
